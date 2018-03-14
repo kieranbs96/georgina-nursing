7853232250e11d40ee95fe064efa0df7ca8d7262
kieranbs96 <kieranbs96@gmail.com>
2018-03-14 09:17:42 +0000

    Sitemap and Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,21 @@
 module.exports = {
   siteMetadata: {
     title: 'Georgina Cross - Registered Nurse - UK',
+    siteUrl: 'https://www.georginacross.co.uk',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-plugin-sitemap',
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-115759724-1',
+        head: false,
+      },
+    },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
